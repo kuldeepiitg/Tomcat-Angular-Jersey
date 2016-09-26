87a06325719a2b302b09b5c345b5e2d257f931bf
Kuldeep Yadav <kuldeepwrites@gmail.com>
2016-09-26 08:03:03 +0530

    Project Initial setup  1. Project is created using Jersey-quickstart-archetype  2. MyResource is created by default  3. Using Bower angular is added.

---
+++ src/main/webapp/app/app.controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kuldeep on 25/09/16.
+ */
+var app = angular.module('app');
+
+app.controller('appCtrl', function ($scope) {
+    $scope.greet = "Hello World!!";
+});
---
+++ src/main/webapp/app/app.module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kuldeep on 25/09/16.
+ */
+angular.module('app', ['app.orders']);
